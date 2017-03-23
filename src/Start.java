c04de39f8575dad90b0290321f9a3ada9208ce8d
Peter Schriever <peterzen72@gmail.com>
2017-03-23 11:17:46 +0100

    hello world added

---
+++ src/Start.java
@@ -0,0 +1,9 @@
+/**
+ * Created by peterzen on 2017-03-23.
+ * Part of the tictactoe project.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
